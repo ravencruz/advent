93010d1abf4df1a09d194d8f89e0a75f3378c24f
reyban <reyban@gmail.com>
2021-09-11 23:57:45 -0300

    feat: solution n*n

--- src/main/kotlin/twenty/zero/report_repair.kt
+++ src/main/kotlin/twenty/zero/report_repair.kt
@@ -1,52 +1,35 @@
 package twenty.zero
 
-//fun readFileAsLinesUsingUseLines(fileName: String): List<String>
-//        = File(fileName).useLines { it.toList() }
+import java.io.File
 
-fun readFileAsLinesUsingUseLines(fileName: String): String =
-    object {}.javaClass.getResource("/report_input.txt").readText()
 
 fun main() {
     println("hello")
-    val input = readFileAsLinesUsingUseLines("./report_input.txt")
-//    println(input)
 
-    val numbers = input.trim()
-        .split("\\s+".toRegex())
+    val numbers = File("src/main/resources/report_input.txt")
+        .readLines()
         .map { it.toInt() }
 
-//    val sample = listOf(1,2,3, 5)
-//    for (i in sample.indices) {
-//        val next = i+1
-//        for (j in next..sample.lastIndex) {
-//            println("${sample[i]} - ${sample[j]}")
-//        }
-//    }
-
-    var res = -1
-    for (i in numbers.indices) {
-        val next = i + 1
-        for (j in next..numbers.lastIndex) {
-            if (numbers[i] + numbers[j] == 2020) {
-                res = numbers[i] * numbers[j]
-                break
+    abc@
+    for (first in numbers) {
+        for (second in numbers) {
+            if (first + second == 2020) {
+                println(first * second)
+                break@abc
             }
         }
     }
-    println(res)
 
-    for (i in numbers.indices) {
-        val iNext = i + 1
-        for (j in iNext..numbers.lastIndex) {
-            val jNext = j + 1
-            for (k in jNext..numbers.lastIndex) {
-                if (numbers[i] + numbers[j] + numbers[k] == 2020) {
-                    res = numbers[i] * numbers[j] * numbers[k]
-                    break
+    abcd@
+    for (first in numbers) {
+        for (second in numbers) {
+            for (third in numbers) {
+                if (first + second + third == 2020) {
+                    println(first * second * third)
+                    break@abcd
                 }
             }
         }
     }
-    println(res)
 
 }
