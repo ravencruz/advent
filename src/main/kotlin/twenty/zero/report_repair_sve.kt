7139249b451df79a24813d232583ab302e63e3e7
reyban <reyban@gmail.com>
2021-09-19 13:00:06 -0300

    day 1 first part two = 2020

--- src/main/kotlin/twenty/zero/report_repair_sve.kt
+++ src/main/kotlin/twenty/zero/report_repair_sve.kt
@@ -8,23 +8,42 @@ fun main() {
 
     val numbers = File("src/main/resources/simple_input.txt")
         .readLines()
-        .map { it.toInt() }
+        .map(String::toInt)
 
-//    println(numbers.associate { "key$it" to "value$it" })
-//    println(numbers.associateBy { "key$it" })
-//    println(numbers.associateWith { "value$it" })
+    val sum = 2020
+    val pair = findPairOfSum(numbers, sum)
 
-    val complements = numbers.associateBy { 2020 - it }
-    println(numbers)
-    println(complements)
+    println(pair)
+    println(pair.first * pair.second)
+//
+//    val sum3 = 2020
+//    val pair31 = findPairOfSum(numbers, sum)
+}
 
-    val pair = numbers.mapNotNull { number ->
+fun findPairOfSum(numbers: List<Int>, sum: Int): Pair<Int, Int> {
+    val complements = numbers.associateBy { sum - it }
+
+    val pair = numbers.firstNotNullOf { number ->
         val complement = complements[number]
         if (complement != null)
             Pair(number, complement)
         else null
-    }.first()
+    }
 
-    println(pair)
-    println(pair.first * pair.second)
+    return pair
 }
+
+//    println(numbers.associate { "key_$it" to "value_$it" })
+//    println(numbers.associateBy { "key_$it" })
+//    println(numbers.associateWith { "value_$it" })
+
+//val complements = numbers.associateBy { 2020 - it }
+//println(numbers)        //become values
+//println(complements)    // become key
+//
+//val res = complements.mapNotNull { (key, value) ->
+//    if (numbers.contains(key)) {
+//        key * value
+//    } else null
+//}.first()
+//println(res)
